d33037831d4c52138db309c7081e5a506dce7666
Jen Wachter <jwachter@jhu.edu>
2013-02-26 13:23:42 -0500

    Added some docuemntation

--- src/RouterExchange/Interfaces/Router.php
+++ src/RouterExchange/Interfaces/Router.php
@@ -7,7 +7,6 @@ interface Router
 	// Settings
 	public function setDebug($boolean);
 
-
 	// HTTP methods
 	public function get($pattern, $callback);
 	public function post($pattern, $callback);
@@ -15,15 +14,19 @@ interface Router
 	public function delete($pattern, $callback);
 	public function options($pattern, $callback);
 
+	// Route methods
 	public function name($name);
 	public function conditions($array);
 
+	// Router helpers
 	public function redirect($url);
 	public function halt();
 	public function pass();
 	public function stop();
 
+	// Error reporting
 	public function error($callable);
 
+	// Run the router
 	public function run();
 }
